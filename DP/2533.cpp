0d61a6d4bfe679de457cecc9132d3b457ef51bf2
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-09-01 18:06:53 +0900

    사회망서비스 업로드

--- DP/2533.cpp
+++ DP/2533.cpp
@@ -3,36 +3,38 @@
 #include <string.h>
 using namespace std;
 #define Max 1000001
+int min(int a, int b) { return a > b?b:a; };
 int N;
 vector<int> Graph[Max];
-int cache[Max];
-int dp(int cur,int from, bool state)
+int cache[Max][2];
+int dp(int cur, int from, int state)
 {
-    if (Graph[cur].size() == 1)
+    if (Graph[cur].size() == 1 && from == Graph[cur][0]) //말단노드에 도착했을경우
     {
-        if (state == true)
+        if (state == 1) //자기자신칠해야하는경우 1
             return 1;
         else
-            return 0;
+            return 0; //자기자신안칠해도되는경우
     }
-    int &result = cache[cur];
+    int& result = cache[cur][state];
     if (result != -1)
         return result;
     else
-        result = 0;
-    result ++;
+        result = 1;//자기 자신 칠할경우
     for (int i = 0; i < Graph[cur].size(); i++)
-    {   if(Graph[cur][i]==from) continue;
-        result += (dp(Graph[cur][i],cur,false));
+    {
+        if (Graph[cur][i] == from) continue;
+        result += (dp(Graph[cur][i], cur, 0)); //state=0으로
     }
-    if (state == false)
+    if (state == 0) //이때 자기자신 안칠해도 되는경우
     {
         int k = 0;
         for (int i = 0; i < Graph[cur].size(); i++)
-        {   if(Graph[cur][i]==from) continue;
-            k += dp(Graph[cur][i],cur,true);
+        {
+            if (Graph[cur][i] == from) continue; //이떄 왔던 노드일경우 스킵
+            k += dp(Graph[cur][i], cur, 1); //다음노드들은 자기자신을 칠해야한다.
         }
-        result = min(k, result);
+        result = min(k, result); //자기자신 칠한경우+자기자신안칠한경우
     }
     return result;
 }
@@ -49,6 +51,6 @@ int main()
         Graph[to].push_back(from);
     }
     memset(cache, -1, sizeof(cache));
-    cout << min(dp(1,Graph[1][0],true), dp(1,Graph[1][0],false) + 1);
+    cout <<dp(1,0,0);
     return 0;
 }
