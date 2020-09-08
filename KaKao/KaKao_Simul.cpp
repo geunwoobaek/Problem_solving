1e085eaaa3ae8dca42b9d01c1bbabf1a0226a719
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-09-09 02:09:09 +0900

    Update KaKao_Simul.cpp
    기둥과 보 설치

--- KaKao/KaKao_Simul.cpp
+++ KaKao/KaKao_Simul.cpp
@@ -12,50 +12,79 @@ vector<vector<int>> solution(int n, vector<vector<int>> build_frame)
     vector<vector<int>> answer;
     return answer;
 }
-void simulation(int limit,vector<int> &command)
+//command[2]=대상
+void simulation(int limit, vector<int> &command)
 {
     int x = command[0];
     int y = command[1];
+    //보는 항상 기둥위에있다 기둥의 y=k 보는 k+1
     if (command[3] == '0') //삭제
     {
-        if (Map[x][y]!=command[2]||Map[x][y]!=2) //아무것도 삭제할게 없을경우에 끝냄
-            return;          
+        if (Map[x][y] != command[2] || Map[x][y] != 2) //아무것도 삭제할게 없을경우에 끝냄
+            return;
         if (command[2] == 0) //기둥
         {
-          if(Map[x][y+1]==-1&&Map[x-1][y+1]!=1) //위에 아무것도없을시
-          {
-            Map[x][y]--;
-          }
-          else{ //위에 뭐가있을경우
-           //위의 왼쪽에 보가 있을경우 :그왼쪽에 기둥또는 더왼쪽에 보가있어야함
-           if(x>0&&(Map[x-1][y+1]==1)) return;
-           //위의 오른쪽에 보가 있을경우 :그오른쪽에 기둥이 있거나 그오른쪽에 보가 있어야함
-           if(x<limit-1&&(Map[x][y+1]==1&&Map[x][y]) return;
-           //위의 바로위에 기둥이 있을경우 :해당지점의 오른쪽또는 왼쪽에 보가 있어야함
-            if(Map[x][y+1]==0)
+            int &nowColum = Map[x][y];
+            if (y == limit - 1)
+                return;                                                    //범위넘어설때
+            if (Map[x][y + 1] == -1 && (x == 0 || Map[x - 1][y + 1] != 1)) //위에 아무것도없을시
             {
-                if(x>0&&Map[x-1][y+1]<1) return ;
+                nowColum--;
             }
-            Map[x][y]--;
-          }
+            else //위에 뭐가있을경우
+            {
+                //위에 두개가 다있을경우
+                if (x > 0 && Map[x][y + 1] != -1 && Map[x - 1][y + 1] != -1)
+                {
+                    int right = x;
+                    int left = x - 1;
+                    bool right_condition = false;
+                    bool left_condition = false;
+                    while (Map[right][y + 1] != -1)
+                    {
+                        if (Map[right][y + 1] == 0 || Map[right++][y + 1] == 2)
+                        {
+                            right_condition = true;
+                            break;
+                        }
+                    }
+                    if (!right_condition)
+                        return;
+                    while (left > 0 && Map[left][y + 1] != -1)
+                    {
+                        if (Map[left][y + 1] == 0 || Map[left--][y + 1] == 2)
+                        {
+                            left_condition = true;
+                            break;
+                        }
+                    }
+                     if (!left_condition)
+                        return;
+                }
+                //위에 하나만있을경우
+                else
+                {
+                    if ((x > 0 && Map[x - 1][y + 1] == 1) && (Map[x - 1][y] != 2 || Map[x - 1][y] != 0))
+                        return; //왼쪽기둥
+                    else if (Map[x - 1][y] != 0 && Map[x - 1][y] != 2)
+                        return; //오른쪽기둥
+                }
+            }
+            nowColum--;
         }
         else //보
         {   
-            //왼쪽에 보가있는지 체크 
-            //있다면 보만 있으면 삭제 x
-            if(x>0&&Map[x-1][y]==1) return ;
-            //오른쪽에 보가 있는지 체크
-            //오른쪽에 보만있는경우 삭제 x
-            if(x<limit-1&&Map[x+1][y]==1) return ;
-            //보의 위에 기둥이 있는지 체크
-            //있다면 다른 바칠곳이 있는지 체크
-      
-            //보위에 기둥이있다면
-            //보의 오른쪽부분에 기둥이 있는경우 : 기둥의 밑부분에 기둥또는 다른 보가 있어야한다.
-            if(Map[x+1][y]==1 &&(Map[x+1][y-1]!=0&&Map[x+1][y-1]!=2)) return ;
-            //보의 왼쪽부분에 기둥이있는경우 :;;
-            if(Map[x][y]==1 &&(Map[x][y-1]!=0&&(Map[x-1][y]<1))) return ;
+            int& Road=Map[x][y];
+            //삭제할때 위에 뭐가있는지 확인
+            if(Road==2) //왼쪽위벽검사
+            {
 
+            }
+            if(x>0&&Map[x+1][y]==0) //오른쪽위벽검사
+            {
+
+            }
+            //삭제할때 양옆에 뭐가 있는지 확인
         }
     }
     else //설치
@@ -69,28 +98,27 @@ void simulation(int limit,vector<int> &command)
             {
                 Map[x][y]++;
             }
-            else 
-            {   //바닥설치가 아닌경우
-                if(Map[x][y-1]==0||Map[x][y-1]==2) //밑에 기둥이있다면
-                {
-                    Map[x][y]++;
-                }
-                else if(Map[x][y]==1||(x>0&&Map[x-1][y]>=1))//밑에 보가있다면
+            else
+            {                                                                  //바닥설치가 아닌경우
+                if (Map[x][y - 1] != -1 || (x > 0 && Map[x - 1][y - 1] != -1)) //밑에 무엇이라도 있다면
                 {
                     Map[x][y]++;
                 }
             }
         }
         else //보
-        { 
-            if(y==0) return; //바닥이라면
-            if(Map[x][y-1]==0&&(x<limit-1&&Map[x+1][y]==0)) //밑에 기둥이있다면
+        {
+            if (y == 0)
+                return;                                                          //바닥이라면
+            if (Map[x][y - 1] == 0 && (x < limit - 1 && Map[x + 1][y - 1] == 0)) //밑에 기둥이있다면
             {
-                Map[x][y]++; 
+                Map[x][y]++;
             }
-            else{
-                if(x==0) return; 
-                if(Map[x-1][y]>=0&&Map[x+1][y]>=0) //양옆에 보가있다면
+            else
+            {
+                if (x == 0 || x == limit - 1)
+                    return;
+                if (Map[x - 1][y] >= 0 && Map[x + 1][y] >= 0) //양옆에 보가있다면
                 {
                     Map[x][y]++;
                 }
