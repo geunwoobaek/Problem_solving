c30f0c8335a3b772f1fde37e66301f5c2ca7c6d5
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-09-27 17:12:04 +0900

    _
    _

--- .vscode/Naver_2020_2/spilt.cpp
+++ .vscode/Naver_2020_2/spilt.cpp
@@ -2,32 +2,33 @@
 #include <vector>
 using namespace std;
 
-vector<string> spilt(string &str,string &pattern)
+vector<string> spilt(const string& str,const string& pattern) //직접spilt구현
 {
     vector<string> answer;
-    int start=0;
-    for(int i=0;i<str.length();i++)
+    char _last = pattern.back();
+    int _start = 0;
+    int _size = pattern.length();
+    for (int i = 0;i < str.length();i++)
     {
-        if(str[i]=' ')//patern
+        if (str[i]== _last&&i>=0&&str.substr(i-(_size-1), _size)==pattern)//pattern
         {
-            if(i!=start)
-            {
-                answer.push_back(str.substr(start,i-start));
-            }
-            start=i+1;
+          int size = i - (_size - 1) - _start;
+          if(size>0)
+          answer.push_back(str.substr(_start,i- (_size - 1) -_start));
+          _start = i+1;
         }
     }
-    if(start!=str.length())
-    {
-        answer.push_back(str.substr(start,str.length()-start));
+    if (_start != str.length()) {
+        answer.push_back(str.substr(_start, str.length() - _start));
     }
     return answer;
 }
 
 int main()
 {
-string test=" abcd sdfc fcdsef fesa";
-vector<string> vec=spilt(test,test);
-return 0;
+    string test = " kdcd__sdab__fcdsabef_fesa";
+    string pattern = "_";
+    vector<string> vec = spilt(test, pattern);
+    return 0;
 }
 
