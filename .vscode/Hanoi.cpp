c85ba15a2179475c16c4bfb77c4927933feccb6e
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-10-05 21:35:56 +0900

    _
    _

---
+++ .vscode/Hanoi.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define f(i,j,k) for(int i=j;i<=k;i++)
+int N, G;
+vector<vector<int>> tower(4);
+void input();
+void solve();
+int main()
+{
+    solve();
+}
+void input()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N >> G;
+    f(i,1,3)
+    {
+        int t;
+        cin >> t;
+        tower[i].resize(t);
+        f(j,0,t-1)
+        {
+            cin>>tower[i][j];
+        }
+    }
+}
+void solve(){
+    input();
+    cout<<"";
+}
---
+++ .vscode/eee.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class problem2438 {
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+
+            int a = in.nextInt();
+
+            for(int i = 1; i <= a; i++) {
+            System.out.println( i );
+            }
+    }
+}
