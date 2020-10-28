fa70a5df73252103ccaeeddb175a69b9093c2fd3
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-10-28 18:41:28 +0900

    _
    _

--- Samsung/simulation/12100.cpp
+++ Samsung/simulation/12100.cpp
@@ -8,10 +8,9 @@ using namespace std;
 int N, _max = 0;
 vector<vector<int>> board(21, vector<int>(21));
 void input();
-void simul(vector<vector<int>> &bor, int depth, int dir);
+void simul(vector<vector<int>> &cur, int depth);
 void solve();
-void move(vector<vector<bool>> &ismerge, vector<vector<int>> &cur, int dx_, int dy_, int x, int y);
-bool ismerge[21][21];
+void move(vector<vector<int>> &cur, int i);
 int dy[4] = {-1, 1, 0, 0};
 int dx[4] = {0, 0, -1, 1};
 int main()
@@ -22,7 +21,7 @@ int main()
 void solve()
 {
     input();
-    simul(board, 5, -1);
+    simul(board, 5);
     cout << _max;
 }
 void input()
@@ -34,14 +33,14 @@ void input()
         for (int j = 1; j <= N; j++)
             cin >> board[i][j];
 }
-void simul(vector<vector<int>> &bor, int depth, int dir) //
+void simul(vector<vector<int>> &cur, int depth) //
 {
     if (depth == 0)
     {
         for (int i = 1; i <= N; i++)
             for (int j = 1; j <= N; j++)
-                if (bor[i][j] > _max)
-                    _max = bor[i][j];
+                if (cur[i][j] > _max)
+                    _max = cur[i][j];
         return;
     }
     //위로올라갈때 dir=0 //y=1,x=1인지점부터 y=N까지 dy=-1
@@ -50,35 +49,33 @@ void simul(vector<vector<int>> &bor, int depth, int dir) //
     //오른쪽으로 dir=3 //x=N부터 X=1까지 dx=+1
     for (int i = 0; i < 4; i++)
     {
-        vector<vector<int>> cur = bor; //board복사
-        vector<vector<bool>> ismerge(N + 1, vector<bool>(N + 1));
-        if (i % 2 == 0)
-        {
-            for (int y = 1; y <= N; y++) //1인지점부터 N지점까지확인
-                for (int x = 1; x <= N; x++) move(ismerge,cur,dx[i],dy[i],x,y);
-        }
-        else
-        {
-            for (int y = N; y >= 0; y--)
-                for (int x = N; x >= 0; x--) move(ismerge,cur,dx[i],dy[i],x,y);
-        }
-        simul(cur, depth - 1, i);
+        move(cur,i,depth);
     }
 }
-void move(vector<vector<bool>> &ismerge, vector<vector<int>> &cur, int dx_, int dy_, int x, int y)
+void move(vector<vector<int>> cur, int i,int depth)
 {
-    int curx = x, cury = y;
-    if (cur[y][x] == 0)
-        return ;
-    while (curx + dx_ >= 1 && cury + dy_ >= 1 && curx + dx_ <= N && cury + dy_ <= N && !ismerge[cury + dy_][curx + dx_] && (cur[cury + dy_][curx + dx_] == 0 || (cur[cury + dy_][curx + dx_] == cur[y][x])))
+    vector<vector<bool>> ismerge(N + 1, vector<bool>(N + 1));
+    int x, y, add;
+    if (i % 2 == 0) x = y = add = 1;
+    else x = y = N,add = -1;
+    for (; y <= N && y >= 1; y += add)
     {
-        curx += dx_;
-        cury += dy_;
+        for (; x <= N && x >= 1; x += add)
+        {   if (cur[y][x] == 0)
+                continue;
+            int curx = x, cury = y;
+            while (curx + dx[i] >= 1 && cury + dy[i] >= 1 && curx + dx[i] <= N && cury + dy[i] <= N && !ismerge[cury + dy[i]][curx + dx[i]] && (cur[cury + dy[i]][curx + dx[i]] == 0 || (cur[cury + dy[i]][curx + dx[i]] == cur[y][x])))
+            {
+                curx += dx[i];
+                cury += dy[i];
+            }
+            if (cury != y || x != curx)
+            {
+                ismerge[cury][curx] = cur[cury][curx] != 0;
+                cur[cury][curx] += cur[y][x];
+                cur[y][x] = 0;
+            }
+        }
     }
-    if (cury != y || x != curx)
-    {
-        ismerge[cury][curx] = cur[cury][curx] != 0;
-        cur[cury][curx] += cur[y][x];
-        cur[y][x] = 0;
-    }
+    simul(cur, depth - 1);
 }
