a76de0c460d25db3ebc3b97fd2d7699a035e1c18
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-11-07 12:50:49 +0900

    _
    _

--- Samsung/simulation/19236.cpp
+++ Samsung/simulation/19236.cpp
@@ -11,17 +11,14 @@ int Maximum = 0;
 void solve();
 void input();
 void simulation();
-void dfs(Pair shark, vector<vector<int>> sea_);
+void dfs(Pair shark);
 int dx[9] = { 0,0, -1, -1, -1, 0, 1, 1, 1 };
-int dy[9] = { 0,1, 1, 0, -1, -1, 0, 0, 1 };
+int dy[9] = { 0,-1, -1, 0, 1, 1, 1, 0, -1 };
 vector<vector<int>> sea(5, vector<int>(5)); //물고기번호담겨있음
 int answer = 0;
 Pair Shark = { 1,{1,0} };
 vector<Pair> fish(17);
-enum Direct
-{
-    ZERO, U, LU, L, LD, D, RD, R, RU
-};
+
 int main()
 {
     solve();
@@ -45,15 +42,16 @@ void input()
         }
 }
 void simulation() {
-    dfs(Shark, sea);
+    dfs(Shark);
 }
-void dfs(Pair shark, vector<vector<int>> sea_)
+void dfs(Pair shark)
 {
     vector<Pair> fish_ = fish;
-    int now = sea_[shark.y][shark.x];
+    vector<vector<int>> sea_ = sea;
+    int now = sea[shark.y][shark.x];
     shark.size_ += now;
     int shark_dir = fish[now].dir;
-    sea_[shark.y][shark.x] = 0;
+    sea[shark.y][shark.x] = 0;
     fish[now] = { 0,{0,0} };
     Maximum = max(Maximum, shark.size_);
 
@@ -66,20 +64,22 @@ void dfs(Pair shark, vector<vector<int>> sea_)
         {
             fish[i] = { prev.y + dy[j],{prev.x + dx[j],j} }; //위치변경
             if (fish[i].y > 0 && fish[i].x > 0 && fish[i].y <= 4 && fish[i].x <= 4 && (shark.y != fish[i].y || shark.x != fish[i].x)) break;
-            if (j == 8) j = 1;
+            j %= 8;
         }
-        int changed_fish_num = sea_[fish[i].y][fish[i].x]; //변경될위치에갈 물고기번호
+        int changed_fish_num = sea[fish[i].y][fish[i].x]; //변경될위치에갈 물고기번호
         //map변경
-        sea_[fish[i].y][fish[i].x] = i;
-        sea_[prev.y][prev.x] = changed_fish_num;
+        sea[fish[i].y][fish[i].x] = i;
+        sea[prev.y][prev.x] = changed_fish_num;
         if (changed_fish_num == 0) continue; //물고기가업을떄
+        prev.dir = fish[changed_fish_num].dir;
         fish[changed_fish_num] = prev; //물고기위치변경완료
     }
     for (int i = 1;i < 4;i++)
     {
-        Pair next = { shark.y + dy[shark_dir * i],{shark.x + dx[shark_dir * i],shark.size_} };
-        if (next.y > 0 && next.x > 0 && next.y <= 4 && next.x <= 4 && sea_[next.y][next.x] != 0)
-            dfs(Shark, sea_);
+        Pair next = { shark.y + i*dy[shark_dir],{shark.x + i*dx[shark_dir],shark.size_} };
+        if (next.y > 0 && next.x > 0 && next.y <= 4 && next.x <= 4 && sea[next.y][next.x] != 0)
+            dfs(next);
     }
     fish = fish_;
+    sea = sea_;
 }
