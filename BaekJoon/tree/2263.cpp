8674a57456ad0978840195f2ebed876788af876a
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-12-02 23:34:39 +0900

    _
    _

--- BaekJoon/tree/2263.cpp
+++ BaekJoon/tree/2263.cpp
@@ -9,7 +9,10 @@ int N;
 int In_Order[MaxSize];
 int Post_Order[MaxSize];
 
-int number(range A) { return A.end - A.start+1; }
+int number(range A) {
+    int num = (A.end - A.start + 1);
+    return num > 0 ? num : 0;
+}
 //현재까지 탐색한 InOrder의 범위를 가지고있어야함
 //현재까지 탐색한 PostOrder의 범위도 가지고 있어야함.
 
@@ -19,8 +22,8 @@ int number(range A) { return A.end - A.start+1; }
 //3.in_range안의 A의 왼쪽과 오른쪽의 갯수를 찾음
 //4.오른쪽갯수가 존재할경우 해당범위 재지정및 1~4,(오른쪽갯수+1)왼쪽출력(1~4)
 void FindChildNode(range in_range, range post_range)
-{
-    int left_num=0, right_num=0;
+{   
+    int left_num = 0, right_num = 0;
     range in_left, in_right, post_left, post_right;
     in_left = in_right = in_range;
     int mid = Post_Order[post_range.end];
@@ -29,15 +32,15 @@ void FindChildNode(range in_range, range post_range)
     in_right.start = In_Order[mid] + 1;
     left_num = number(in_left);
     right_num = number(in_right);
-    if (right_num >0)
+    if (left_num > 0)
     {
-        post_right.end -= 1, post_right.start = post_range.end - right_num;
-        FindChildNode(in_right,post_right);
+        post_left.end = post_range.end - 1 - right_num, post_left.start = post_left.end + 1 - left_num;
+        FindChildNode(in_left, post_left);
     }
-    if (left_num >0)
+    if (right_num > 0)
     {
-        post_left.end = post_range.end-1-right_num, post_left.start =  post_left.end+1-left_num;
-        FindChildNode(in_left,post_left);
+        post_right.end = post_range.end-1, post_right.start = post_range.end - right_num;
+        FindChildNode(in_right, post_right);
     }
 }
 int main(void)
@@ -58,6 +61,6 @@ int main(void)
         In_Order[k] = i; //위치를담고있다.
     }
     f(i, 1, N) cin >> Post_Order[i];
-    FindChildNode({1,N},{1,N});
+    FindChildNode({ 1,N }, { 1,N });
     return 0;
 }
