0a53108cf17dbbea6f4383095d44148d154ce89b
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2021-01-03 14:59:35 +0900

    _
    _

--- .vscode/KaKao/02.cpp
+++ .vscode/KaKao/02.cpp
@@ -1,37 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define F(i, j, k) for (int i = j; i <= k; i++)
-#define LEFT 0
-#define RIGHT 1
+#define LEFT -1
+#define RIGHT +1
 #define y first
 #define x second
-pair<int, int> start_idx;
-vector<int> _Lock,_Key;
-int key_Size, lock_Size,Full;
-void makeBit(const vector<vector<int>> &key, const vector<vector<int>> &lock);
-void goLeft(vector<int> &key) { start_idx.x -= 1; }
-void goRight(vector<int> &key) { start_idx.x += 1; }
-void goUp(vector<int> &key) { start_idx.y -= 1; }
-void goDown(vector<int> &key) { start_idx.y += 1; }
-void goRotate(vector<int> &key);
-bool isCorrect(const vector<int> &key);
-bool checkAllRow(vector<int> key, int cmd);
-bool solution(vector<vector<int>> key, vector<vector<int>> lock);
-// int main()
-// {
-//     vector<vector<int>> key = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};//true
-//     vector<vector<int>> key2 = {{1,1 ,1}, {1, 1, 1}, {0, 1, 1}}; //false
-//     vector<vector<int>> key3 = {{1, 1, 0}, {1, 1, 1}, {1, 1, 1}}; //true
-//     vector<vector<int>> lock = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
-//     // if (solution(key, lock))
-//     //     cout << "true";
-//      if (solution(key2, lock))
-//         cout << "true";
-//      if (solution(key3, lock))
-//         cout << "true";
-//     else cout<<"false";
-// }
-
+pair<int, int> start_idx; //key를 읽어드릴 시작지점
+vector<int> _Lock, _Key;
+int key_Size, lock_Size, Full;
+void makeBit(const vector<vector<int>> &key, const vector<vector<int>> &lock); //비트로바꾸기
+void goRotate(vector<int> &key);                                               //키 회전시키기
+bool isCorrect(const vector<int> &key);                                        //맞는지 검사
+bool checkAllRow(const vector<int> &key);                                      //해당렬 전부검사
+bool checkDirection(vector<int> key, int cmd);                                 //해당 index에서 해당 방향검사
 bool solution(vector<vector<int>> key, vector<vector<int>> lock)
 {
     key_Size = key.size();
@@ -40,48 +21,38 @@ bool solution(vector<vector<int>> key, vector<vector<int>> lock)
     _Key.resize(key_Size);
     _Lock.resize(lock_Size);
     makeBit(key, lock); //비트키만들기
+
     F(rotate, 0, 3)
     {
-        if (rotate != 0)
-            goRotate(_Key);
-        if (isCorrect(_Key))
-            return true;
+        start_idx = {0, 0};
+        goRotate(_Key);
+        if (isCorrect(_Key)) return true;
         vector<int> temp = _Key;
-        F(up, 0, key_Size - 2)
+        F(up, 1, key_Size - 1)
         {
-            goUp(temp);
-            if (isCorrect(temp))
-                return true;
-            if (checkAllRow(temp, LEFT))
-                return true;
-            if (checkAllRow(temp, RIGHT))
-                return true;
+            start_idx.y--; //위로
+            if (checkAllRow(temp)) return true;
         }
         temp = _Key;
         start_idx.y = 0;
-        F(down, 0, lock_Size - 2)
+        F(down, 1, lock_Size - 1)
         {
-            goDown(temp);
-            if (isCorrect(temp))
-                return true;
-            if (checkAllRow(temp, LEFT)) //왼쪽으로
-                return true;
-            if (checkAllRow(temp, RIGHT)) //오른쪽으로
+            start_idx.y++; //밑으로
+            if (checkAllRow(temp))
                 return true;
         }
-        start_idx.y = 0;
     }
     return false;
 }
-void makeBit(const vector<vector<int>> &key, const vector<vector<int>> &lock)
+void makeBit(const vector<vector<int>> &key, const vector<vector<int>> &lock) //비트로바꾸기
 {
     F(i, 0, lock_Size - 1)
     {
         F(j, 0, lock_Size - 1)
         {
             if (i < key_Size && j < key_Size)
-                _Key[i] += (key[i][j] <<j);
-            _Lock[i] += (lock[i][j] <<j);
+                _Key[i] += (key[i][j] << j);
+            _Lock[i] += (lock[i][j] << j);
         }
     }
 }
@@ -90,18 +61,22 @@ void goRotate(vector<int> &key)
     vector<int> new_key(key_Size);
     F(i, 0, key_Size - 1)
     F(j, 0, key_Size - 1)
-    new_key[i] += (key[j] & (1 << (key_Size - 1 - i))) != 0 ? (1 << j) : 0;
+        new_key[i] += (key[j] & (1 << (key_Size - 1 - i))) != 0 ? (1 << j) : 0;
     key = new_key;
 }
-bool checkAllRow(vector<int> key, int cmd)
-{   
-    int number=cmd==LEFT?key_Size-2:lock_Size-2;
-    F(k, 0, number) //한쪽으로 쭉탐색
+bool checkAllRow(const vector<int> &key)
+{
+    if (isCorrect(key)) return true;
+    if (checkDirection(key, LEFT)) return true;
+    if (checkDirection(key, RIGHT)) return true;
+    return false;
+}
+bool checkDirection(vector<int> key, int cmd)
+{
+    int number = cmd == LEFT ? key_Size - 1 : lock_Size - 1;
+    F(k, 1, number) //한쪽으로 쭉탐색
     {
-        if (cmd == LEFT)
-            goLeft(key);
-        else if (cmd == RIGHT)
-            goRight(key);
+        start_idx.x += cmd;
         if (isCorrect(key))
             return true;
     }
