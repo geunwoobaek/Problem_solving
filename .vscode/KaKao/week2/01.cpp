a698128fcf0748c72be2c24a557cca25ba1e5a85
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2021-01-08 11:59:08 +0900

    Update 01.cpp

--- .vscode/KaKao/week2/01.cpp
+++ .vscode/KaKao/week2/01.cpp
@@ -8,10 +8,10 @@ bool isVisit[101][101][2] = {
 enum Direction
 {
     Right = 0, //x2>x1
-    Down = 2   //y2>y1
+    Down = 1   //y2>y1
 };
 int dy[4] = { 0, 0, 1, -1 };
-int dx[4] = { 1, -1, 0, 0 }; //오른쪽 위 왼쪽 아래
+int dx[4] = { 1, -1, 0, 0 }; //오른쪽 왼쪽 아래 위
 enum Rotate
 {
     Clock = 0,
@@ -32,19 +32,20 @@ int solution(vector<vector<int>> board)
     MapSize = board.size();
     return search(board);
 }
-int main()
-{
-    vector<vector<int>> board = { {0, 0, 0, 1, 1}, {0, 0, 0, 1, 0}, {0, 1, 0, 1, 1}, {1, 1, 0, 0, 1}, {0, 0, 0, 0, 0} };
-    cout << solution(board);
-}
+// int main()
+// {
+//     //vector<vector<int>> board = { {0, 0, 0, 1, 1}, {1, 0, 0, 1, 0}, {0, 1, 0, 1, 1}, {1, 1, 0, 0, 1}, {0, 0, 0, 0, 0} };
+//     vector<vector<int>> board = { {0, 0, 1, 1, 1}, {0, 0, 1, 1, 1}, {0, 1, 1, 1, 1}, {0, 0, 1, 1, 1}, {0, 0, 0, 0, 0} };
+//     cout << solution(board);
+// }
 
 bool Rotate(Robot& robot, int Mode, const Board& board)
-{
+{   
     if (Mode == Clock)
     { //시계방향
         if (robot.x1 == robot.x2)
         {
-            if (robot.x1 == MapSize-1|| board[robot.x1 +1][robot.y1] == 1)
+            if (robot.x1 == MapSize-1|| board[robot.y1][robot.x1 +1] == 1)
                 return false;
             robot.direct = Right;
             robot.y1 = robot.y2;
@@ -52,7 +53,7 @@ bool Rotate(Robot& robot, int Mode, const Board& board)
         }
         else //y같을때
         {
-            if (robot.y1 == 0|| board[robot.x1][robot.y1-1] == 1)
+            if (robot.y1 == 0|| board[robot.y1-1][robot.x1] == 1)
                 return false;
             robot.direct = Down;
             robot.x1 = robot.x2;
@@ -63,7 +64,7 @@ bool Rotate(Robot& robot, int Mode, const Board& board)
     { //반시계방향
         if (robot.x1 == robot.x2)
         {
-            if (robot.x1 ==0 || board[robot.x1 - 1][robot.y1] == 1)
+            if (robot.x1 ==0 || board[robot.y1][robot.x1 - 1] == 1)
                 return false;
             robot.direct = Right;
             robot.y1 = robot.y2;
@@ -71,7 +72,7 @@ bool Rotate(Robot& robot, int Mode, const Board& board)
         }
         else
         {
-            if (robot.y1 ==MapSize-1 || board[robot.x1][robot.y1 + 1] == 1)
+            if (robot.y1 ==MapSize-1 || board[robot.y1 + 1][robot.x1] == 1)
                 return false;
             robot.direct = Down;
             robot.x1 = robot.x2;
@@ -82,33 +83,33 @@ bool Rotate(Robot& robot, int Mode, const Board& board)
 }
 bool isRange(Robot& robot, const Board& board)
 {
-    return robot.x1 >= 0 && robot.y1 >= 0 && robot.x2 < MapSize && robot.y2 < MapSize && board[robot.x1][robot.y1] != 1 && board[robot.x2][robot.y2] != 1;
+    return robot.x1 >= 0 && robot.y1 >= 0 && robot.x2 < MapSize && robot.y2 < MapSize && board[robot.y1][robot.x1] != 1 && board[robot.y2][robot.x2] != 1;
 }
+
 int search(const Board& board)
 {
     robots.push(Robot());
     while (!robots.empty())
     {
         Robot robot = robots.front();
-        robots.pop();
-        if ((robot.x1 == MapSize - 1 && robot.y1 == MapSize - 1) || (robot.x2 == MapSize - 1 && robot.y2 == MapSize - 1))
+        robots.pop();    
+        if ((robot.x2 == MapSize - 1 && robot.y2 == MapSize - 1))
         {
             return robot.sec;
         }
         //Go
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 4; i++)
         {
             Robot next_robot;
-            next_robot.x1 = robot.x1 + dx[robot.direct + i];
-            next_robot.x2 = robot.x2 + dx[robot.direct + i];
-            next_robot.y1 = robot.y1 + dy[robot.direct + i];
-            next_robot.y2 = robot.y2 + dy[robot.direct + i];
+            next_robot.x1 = robot.x1 + dx[i];
+            next_robot.x2 = robot.x2 + dx[i];
+            next_robot.y1 = robot.y1 + dy[i];
+            next_robot.y2 = robot.y2 + dy[i];
             next_robot.sec = robot.sec + 1;
             next_robot.direct = robot.direct;
-
-            if (isRange(next_robot, board) && !isVisit[next_robot.x1][next_robot.y1][next_robot.direct])
-            {
-                isVisit[next_robot.x1][next_robot.y1][next_robot.direct] = true;
+            if (isRange(next_robot, board) && !isVisit[next_robot.y1][next_robot.x1][next_robot.direct])
+            {   
+                isVisit[next_robot.y1][next_robot.x1][next_robot.direct] = true;
                 robots.push(next_robot);
             }
         }
@@ -117,9 +118,9 @@ int search(const Board& board)
         {
             Robot next_robot = robot;
             next_robot.sec += 1;
-            if (Rotate(next_robot, mode, board) && isRange(next_robot, board) && !isVisit[next_robot.x1][next_robot.y1][next_robot.direct])
+            if (Rotate(next_robot, mode, board) && isRange(next_robot, board) && !isVisit[next_robot.y1][next_robot.x1][next_robot.direct])
             {
-                isVisit[next_robot.x1][next_robot.y1][next_robot.direct] = true;
+                isVisit[next_robot.y1][next_robot.x1][next_robot.direct] = true;
                 robots.push(next_robot);
             }
         }
