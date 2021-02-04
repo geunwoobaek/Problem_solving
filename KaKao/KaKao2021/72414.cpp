9839d03b078ccab536772be10b4be10430108306
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2021-02-04 19:31:52 +0900

    광고_삽입

--- KaKao/KaKao2021/72414.cpp
+++ KaKao/KaKao2021/72414.cpp
@@ -1,9 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct Record
-{
-    int count, time;
-} record; //첫번째는 count 두번째는 시간
 int convert(string str)
 {
     int hour = stoi(str.substr(0)) * 3600;
@@ -11,28 +7,45 @@ int convert(string str)
     int sec = stoi(str.substr(6));
     return hour + min + sec;
 }
+int sum[3600000];
+struct compare
+{
+    bool operator()(vector<int> a, vector<int> b) { return a[1] > b[1]; };
+};
+struct Point
+{
+    int start = 0, end = 0, sum = 0;
+    int length() { return end - start; }
+} cursor;
+Point record;
+int answer = 2e9;
+string convertTostr(int time)
+{
+    string answer;
+    int hour = time / 3600;
+    int min = (time % 3600) / 60;
+    int sec = (time % 60);
+    string stime[3]{to_string(hour), to_string(min), to_string(sec)};
+    for (auto &t : stime)
+    {
+        if (t.size() == 1)
+            answer += "0";
+        answer += t + ":";
+    }
+    answer.pop_back();
+    return answer;
+}
 vector<vector<int>> logvecs;
-//로그의 시작시간이 적은것부터 담기
-//1. 끝지점이 적은 대로 정렬
-//2. 우선순위큐에 데이터의 시작지점에 대해서 넣기
-//3. 끝점-시작지점이 새로읽는데이터의 시작지점보다 크거나같으면 continue,
-//4. 아닐시 뱉어내기
-//5. MaxCount일시 값저장
+int max_count;
 bool cmp(vector<int> a, vector<int> b)
 {
-    if (a[1] != b[1])
-        return a[1] < b[1];
-    else
-        return a[0] < b[0];
-}
-bool operator<(vector<int> a,vector<int> b){ //끝지점이 작은순으로넣음
-    return a[1]>b[1];
+    return a[0] < b[0];
 }
 string solution(string play_time, string adv_time, vector<string> logs)
 {
-    string answer = "";
     int playtime = convert(play_time);
     int advtime = convert(adv_time);
+    priority_queue<int, vector<int>, greater<>> pq;
     for (auto &log : logs)
     {
         int start_ = convert(log.substr(0, 8));
@@ -40,20 +53,34 @@ string solution(string play_time, string adv_time, vector<string> logs)
         logvecs.push_back({start_, end_});
     }
     sort(logvecs.begin(), logvecs.end(), cmp);
-    priority_queue<vector<int>,vector<vector<int>>> q;
-    int count = 0;
     for (auto &now : logvecs)
-    {
-        count++;
-        q.push(now);
-        while (!q.empty())
+    {   if(now[0]>cursor.end){
+        cursor.start+=now[0]-cursor.end;
+        }
+        pq.push(now[1]);
+        while (!pq.empty())
         {
-            if(q.top()-advtime
+            if (pq.top() < now[0])
+                pq.pop();
+        }
+        for (int i = now[0]; i <= now[1]; i++)
+        {
+            sum[i] = pq.size();
         }
     }
+    return convertTostr(answer > 0 ? answer : 0);
 }
+
 int main()
 {
+    cout << solution("02:03:55", "00:14:15",
+                     {"01:20:15-01:45:14",
+                      "00:40:31-01:00:00",
+                      "00:25:50-00:48:29",
+                      "01:30:59-01:53:29",
+                      "01:37:44-02:02:30"}
 
-    cout << convert("11:11:11");
+    );
+
+    return 0;
 }
