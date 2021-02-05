184715dfced56610639ed080d39b8d7d12571bd6
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2021-02-05 09:42:57 +0900

    플로이드 와샬
    플로이드 와샬

---
+++ KaKao/KaKao2021/Taxi.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//지점갯수 n은 3 이상 200 이하인 자연수입니다. //s첫출발지점 a
+#define F(i, j, k) for (int i = j; i <= k; i++)
+int floaid[301][301];
+int solution(int n, int s, int a, int b, vector<vector<int>> fares)
+{
+    F(i, 1, n)
+    {
+        F(j, 1, n)
+        {
+            floaid[i][j] = 1e9;
+        }
+        floaid[i][i] = 0;
+    }
+    for (auto &fare : fares)
+    {
+        int from = fare[0];
+        int to = fare[1];
+        int cost = fare[2];
+        floaid[from][to]=cost;
+        floaid[to][from]=cost;
+    }
+    F(path, 1, n)
+    {
+        F(start, 1, n) 
+        {  
+            F(to, 1, n) //언젠간 Path를거침
+            {
+                floaid[start][to]=min(floaid[start][to],floaid[start][path]+floaid[path][to]);
+            }
+        }
+    }
+    cout<<"";
+    return 1;
+}
+int main()
+{
+    cout << solution(6, 4, 6, 2, {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}});
+    return 0;
+}
