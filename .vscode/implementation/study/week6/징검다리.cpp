217c2e9ab6b0e15e07b1e663ef9010d523cb5348
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2021-02-23 23:51:54 +0900

    a

--- .vscode/implementation/study/week6/징검다리.cpp
+++ .vscode/implementation/study/week6/징검다리.cpp
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int solution(int distance, vector<int> rocks, int n)
 {
-    int left = 0, right = distance,mid;
-    rocks.push_back(0);
+    int left = 0, right = distance;
     sort(rocks.begin(), rocks.end());
-    while(left<right){
+    rocks.push_back(distance);
+    while(left<right){ 
         int mid=(left+right)>>1;
-        int remove_rocks=0;
-        for(int i=1;i<=rocks.size();++i){
-            int cur=rocks[i]-rocks[i-1];
-            if(cur>mid) remove_rocks--;
-            
+        int remove_rocks=0,before=0; //mid보다 작은 크기일 경우 제거
+        for(int i=0;i<rocks.size()&&remove_rocks<=n;++i){
+            int next_=rocks[i]-before;
+            if(next_<mid) remove_rocks++; //제거
+            else before=rocks[i]; //제거 안했을경우
         }
+        if(remove_rocks>n) right=mid; //rocks가 n개보다 클때 
+        else left=mid+1;//rocks가 n개이거나 n개보다 적을때 
     }
-    
+    return left-1;
 }
 int main()
 {
