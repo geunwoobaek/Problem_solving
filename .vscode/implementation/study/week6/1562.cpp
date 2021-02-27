cef500d20a9791266a84a3beb947b65828650b4d
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2021-02-28 00:00:20 +0900

    _
    _

--- .vscode/implementation/study/week6/1562.cpp
+++ .vscode/implementation/study/week6/1562.cpp
@@ -1,46 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-int N;
-int FULL = (1 << 10) - 1;
-ll cnt = 0;
-ll cache[10][40];
-int dfs(int cur, int idx)
+int n;
+int memo[101][10][1 << 10];
+int solve(int len, int cur, int situ)
 {
-    if (cur < 0 || cur > 10)
-        return 0;
-    if (idx == N)
-        return 1;
-    ll &result = cache[cur][idx];
-    if (result != -1)
-        return result;
-    result = 0;
-    return result = dfs(cur - 1, idx + 1) + dfs(cur + 1, idx + 1);
-}
-int check = 0;
-ll calculate(int cur, int idx, int check)
-{
-    if (cur < 0 || cur > 9 || idx >= N) return 0;
-    check |= (1 << (cur));
-    if (check == FULL)
+    int &ret = memo[len][cur][situ];
+    if (ret != -1)
+        return ret;
+    if (len == n)
     {
-        return cache[cur][idx];
+        if (situ == (1 << 10) - 1)
+            return 1;
+        else
+            return 0;
     }
-    return calculate(cur + 1, idx + 1, check) + calculate(cur - 1, idx + 1, check);
+    ret = 0;
+    if (cur == 0)
+    {
+        ret += solve(len + 1, cur + 1, situ | (1 << (cur + 1)));
+        ret %= 1000000000;
+    }
+    else if (cur == 9)
+    {
+        ret += solve(len + 1, cur - 1, situ | (1 << (cur - 1)));
+        ret %= 1000000000;
+    }
+    else
+    {
+        ret += solve(len + 1, cur - 1, situ | (1 << (cur - 1)));
+        ret %= 1000000000;
+        ret += solve(len + 1, cur + 1, situ | (1 << (cur + 1)));
+        ret %= 1000000000;
+    }
+    return ret %= 1000000000;
+    ;
 }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cin >> N;
-    memset(cache, -1, sizeof cache);
-    for (int i = 0; i <= 9; i++)
-        dfs(i, 0);
-    for (int i = 1; i <= 9; i++)
+    cin >> n;
+    memset(memo, -1, sizeof(memo));
+    int sum = 0;
+    for (int i = 1; i < 10; i++)
     {
-        cnt += calculate(i, 0,i);
-        cnt %= 1000000000;
+        int situ = (1 << i);
+        sum += solve(1, i, situ);
+        sum %= 1000000000;
     }
-    cout << cnt;
+    cout << sum << '\n';
     return 0;
 }
