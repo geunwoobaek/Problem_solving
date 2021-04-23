409553448822cf18790d9cf414215337e1163af7
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2021-04-23 20:44:14 +0900

    hopcroft

--- .vscode/BinaryMatching/hopcroft-karp.cpp
+++ .vscode/BinaryMatching/hopcroft-karp.cpp
@@ -1,57 +1,194 @@
-/*
-1) Initialize Maximal Matching M as empty.
-2) While there exists an Augmenting Path p
-     Remove matching edges of p from M and add not-matching edges of p to M
-     (This increases size of M by 1 as p starts and ends with a free vertex)
-3) Return M. 
-*/
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-//① 교차 경로(alternating path)
-//② 증가 경로(augmenting path)
-vector<vector<int>> u(5, vector<int>()); //u와 이어진 노드들 교차경로+증가경로
-vector<vector<int>> v(5, vector<int>()); //to로 향하는 경로  교차경로+증가경로
-vector<int> alternatingPathFrom(5);
-vector<int> alternatingPathTo(5);
-void addEdge(int from, int to)
-{
-    u[from].push_back({to});
-    v[to].push_back({from});
-}
-void matching()
-{
+#define NIL 0
+#define INF INT_MAX
 
-    for (int i = 1; i <= 5; i++)
+// A class to represent Bipartite graph for Hopcroft
+// Karp implementation
+class BipGraph
+{
+    // m and n are number of vertices on left
+    // and right sides of Bipartite Graph
+    int m, n;
+
+    // adj[u] stores adjacents of left side
+    // vertex 'u'. The value of u ranges from 1 to m.
+    // 0 is used for dummy vertex
+    list<int>* adj;
+
+    // These are basically pointers to arrays needed
+    // for hopcroftKarp()
+    vector<int> pairU, pairV, dist;
+
+public:
+    BipGraph(int m, int n); // Constructor
+    void addEdge(int u, int v); // To add edge
+
+    // Returns true if there is an augmenting path
+    bool bfs();
+
+    // Adds augmenting path if there is one beginning
+    // with u
+    bool dfs(int u);
+
+    // Returns size of maximum matcing
+    int hopcroftKarp();
+};
+
+// Returns size of maximum matching
+int BipGraph::hopcroftKarp()
+{   //증가경로,교차경로
+    // pairU[u] stores pair of u in matching where u
+    // is a vertex on left side of Bipartite Graph.
+    // If u doesn't have any pair, then pairU[u] is NIL
+    pairU.resize(m + 1);
+
+    // pairV[v] stores pair of v in matching. If v
+    // doesn't have any pair, then pairU[v] is NIL
+    pairV.resize(n + 1);
+
+    // dist[u] stores distance of left side vertices
+    // dist[u] is one more than dist[u'] if u is next
+    // to u'in augmenting path
+    dist.resize(m + 1);
+
+    // Initialize NIL as pair of all vertices
+    for (int u = 0; u <= m; u++)
+        pairU[u] = NIL;
+    for (int v = 0; v <= n; v++)
+        pairV[v] = NIL;
+
+    int result = 0;
+
+    while (bfs())
     {
-        for (auto &cur : u[i])
+        // Find a free vertex
+        for (int u = 1; u <= m; u++)
+
+            // If current vertex is free and there is
+            // an augmenting path from current vertex
+            if (pairU[u] == NIL && dfs(u))
+                result++;
+    }
+    return result;
+}
+
+// Returns true if there is an augmenting path, else returns
+// false
+bool BipGraph::bfs()
+{
+    queue<int> Q; //an integer queue
+
+    // First layer of vertices (set distance as 0)
+    for (int u = 1; u <= m; u++)
+    {
+        // If this is a free vertex, add it to queue
+        if (pairU[u] == NIL)
         {
-            if (alternatingPathTo[cur] == 0) //연결 안됐을시
+            // u is not matched
+            dist[u] = 0;
+            Q.push(u);
+        }
+
+        // Else set distance as infinite so that this vertex
+        // is considered next time
+        else dist[u] = INF;
+    }
+
+    // Initialize distance to NIL as infinite
+    dist[NIL] = INF;
+
+    // Q is going to contain vertices of left side only.
+    while (!Q.empty())
+    {
+        // Dequeue a vertex
+        int u = Q.front();
+        Q.pop();
+
+        // If this node is not NIL and can provide a shorter path to NIL
+        if (dist[u] < dist[NIL])
+        {
+            // Get all adjacent vertices of the dequeued vertex u
+            list<int>::iterator i;
+            for (i = adj[u].begin(); i != adj[u].end(); ++i)
             {
-                alternatingPathTo[cur] = i;
-                alternatingPathFrom[i] = cur;
-                break;
+                int v = *i;
+
+                // If pair of v is not considered so far
+                // (v, pairV[V]) is not yet explored edge.
+                if (dist[pairV[v]] == INF)
+                {
+                    // Consider the pair and add it to queue
+                    dist[pairV[v]] = dist[u] + 1;
+                    Q.push(pairV[v]);
+                }
             }
         }
     }
-}
-void search(int )
-void hopcroft_karp(){
-    for(int i:alternatingPathFrom){
-        if(alternatingPathFrom[i]==0){ //만약 매칭된게 없을시
-        dfs(i,true)
 
-        }
-    }
+    // If we could come back to NIL using alternating path of distinct
+    // vertices then there is an augmenting path
+    return (dist[NIL] != INF);
 }
+
+// Returns true if there is an augmenting path beginning with free vertex u
+bool BipGraph::dfs(int u)
+{
+    if (u != NIL)
+    {
+        list<int>::iterator i;
+        for (i = adj[u].begin(); i != adj[u].end(); ++i)
+        {
+            // Adjacent to u
+            int v = *i;
+
+            // Follow the distances set by BFS
+            if (dist[pairV[v]] == dist[u] + 1)
+            {
+                // If dfs for pair of v also returns
+                // true
+                if (dfs(pairV[v]) == true)
+                {
+                    pairV[v] = u;
+                    pairU[u] = v;
+                    return true;
+                }
+            }
+        }
+
+        // If there is no augmenting path beginning with u.
+        dist[u] = INF;
+        return false;
+    }
+    return true;
+}
+
+// Constructor
+BipGraph::BipGraph(int m, int n)
+{
+    this->m = m;
+    this->n = n;
+    adj = new list<int>[m + 1];
+}
+
+// To add edge from u to v and v to u
+void BipGraph::addEdge(int u, int v)
+{
+    adj[u].push_back(v); // Add u to v’s list.
+}
+
+// Driver Program
 int main()
 {
-    addEdge(1, 2);
-    addEdge(1, 3);
-    addEdge(2, 1);
-    addEdge(3, 2);
-    addEdge(4, 2);
-    addEdge(4, 4);
-    matching(); //matching
+    BipGraph g(4, 4);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 1);
+    g.addEdge(3, 2);
+    g.addEdge(4, 2);
+    g.addEdge(4, 4);
+
+    cout << "Size of maximum matching is " << g.hopcroftKarp();
+
     return 0;
 }
